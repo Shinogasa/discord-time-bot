f3e4e736570934d4246f7f3beef448148f25690b
Shinonome <cuulbulu@gmail.com>
2021-08-10 21:52:57 +0900

    Botのメンションの仕方を修正，指定のテキストチャンネルへ投稿するよう修正

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,7 @@
 import discord
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'Your Bot Token'
+TOKEN = 'Your bot token'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -22,22 +22,30 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
 
-        guild = discord.utils.get(client.guilds, name='Your Server')
+        server_name = 'Your server name'
+        voice_channel_name = 'Your voice channel'
+        text_channel_name = 'Your text channel'
+
+        guild = discord.utils.get(client.guilds, name=server_name)
         print(guild)
 
-        channel = discord.utils.get(guild.voice_channels, name='Your Voice channel')
+        voice_channel = discord.utils.get(guild.voice_channels, name=voice_channel_name)
+        
+        text_channel = discord.utils.get(guild.text_channels, name=text_channel_name)
 
-        member = channel.voice_states.keys()
-        print("c " + str(channel))
-        print("member " + str(member))
+        # VCに参加している人のID取得
+        member = voice_channel.voice_states.keys()
 
+        member_mention = []
+
+        # メンション作成
         for i in member :
             print(i)
-            member_mention = "<@" + str(i) + ">"
-            await message.channel.send(f"{member_mention} 寝ろ")
-        await message.channel.send('にゃーん')
+            member = "<@" + str(i) + ">"
+            member_mention.append(member)
+
+        # Botからメンション飛ばす
+        await text_channel.send( str(" ".join(member_mention)) + " そろそろ寝ませんか？")
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
-
-
